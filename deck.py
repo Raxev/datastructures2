db553722e66e31fdf4ecad026ed693ad0467d7cc
Raxev <46421765+Raxev@users.noreply.github.com>
2019-08-14 22:31:32 -0400

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,6 +1,8 @@
 import random
 from card import Card
 from queue_list import Queue
+
+
 class Deck(Queue):
     """
     This class represents a Deck of Cards. This class inherits from
@@ -28,14 +30,11 @@ class Deck(Queue):
                 in the Deck: 13 Cards to a line - The "\n" character 
                 is added for every 13th Card
     """
-    
     def __init__(self):
         """
         This method is the Deck constructor. Call the Queue 
         constructor using super().
         """
-        
-        ## Add your code here ##
         super().__init__()
     
     def initialize(self):    
@@ -45,8 +44,6 @@ class Deck(Queue):
         Suits: 0=Clubs, 1=Diamonds, 2=Hearts, 3=Spades
         Ranks: 1 = Ace, 2-10 = 2-10, 11 = Jacks, 12 = Queen, 13 = King
         """
-        
-        ## Add your code here ##
         for suit in range(4):
             for rank in range(1,14):
                 card = Card(suit,rank)
@@ -56,8 +53,6 @@ class Deck(Queue):
         """
         This method adds a Card to the Deck using enqueue.
         """
-        
-        ## Add your code here ##
         self.enqueue(card)
 
     def deal(self):
@@ -65,8 +60,6 @@ class Deck(Queue):
         This method removes the first Card from the Deck using 
         dequeue.
         """
-        
-        ## Add your code here ##
         return self.dequeue()
 
     def shuffle(self):        
@@ -75,13 +68,9 @@ class Deck(Queue):
         a list as an argument to shuffle the elements of the Queue's 
         Python list and returns the shuffled Deck.
         """
-        
-        ## Add your code here ##
         for times in range(6):
             random.shuffle(self._items)
-
         return self
-
     
     def __str__(self):
         """
@@ -89,13 +78,11 @@ class Deck(Queue):
         13 Cards to a line - The "\n" character is added for every 
         13th Card
         """
-              
-        ## Add your code here ##
         deck_str = ""
         for c in range(len(self._items)):
-            # Get the Card at index c since Deck is
-            # a list of cards.
+            # "Get the Card at index c since Deck is
+            # a list of cards."
             deck_str += "{} ".format(self._items[c])
-            if  (c+1 )% 13 == 0:
+            if (c+1 )% 13 == 0:
                 deck_str += '\n'
         return deck_str
